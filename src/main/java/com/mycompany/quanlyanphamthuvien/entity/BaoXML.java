ec3a043732ca2d3a817e07f82164c90039b38a81
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-04-18 11:58:30 +0700

    Update BaoXML.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/entity/BaoXML.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/entity/BaoXML.java
@@ -3,12 +3,15 @@ package com.mycompany.quanlyanphamthuvien.entity;
 import java.util.ArrayList;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "BaoXML")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class BaoXML {
-
+    @XmlElementWrapper(name = "DanhSachBao")
+    @XmlElement(name = "Bao")
     private ArrayList<Bao> xmlBao;
 
     public ArrayList<Bao> getXmlBao() {
