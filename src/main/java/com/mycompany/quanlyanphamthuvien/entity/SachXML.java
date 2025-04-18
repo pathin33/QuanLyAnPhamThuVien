194c7f43cde3ae8aad5ff7c674f862accc2c500a
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-04-18 11:59:45 +0700

    Update SachXML.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/entity/SachXML.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/entity/SachXML.java
@@ -4,11 +4,15 @@ package com.mycompany.quanlyanphamthuvien.entity;
 import java.util.ArrayList;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "SachXMl")
+@XmlRootElement(name = "SachXML")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class SachXML {
+    @XmlElementWrapper(name = "DanhSachSach")
+    @XmlElement(name = "Sach")
     private ArrayList<Sach> xmlSach;
 
     public ArrayList<Sach> getXmlSach() {
