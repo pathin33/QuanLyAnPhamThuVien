2907c5fb0992cfe516d34e088db946f194eeaa04
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-04-18 11:57:00 +0700

    Update AnPhamXML.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/entity/AnPhamXML.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/entity/AnPhamXML.java
@@ -4,11 +4,15 @@ package com.mycompany.quanlyanphamthuvien.entity;
 import java.util.ArrayList;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "AnPhamXML")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class AnPhamXML {
+    @XmlElementWrapper(name = "DanhSachAnPham")
+    @XmlElement(name = "AnPham")
     private ArrayList<AnPham> xmlAnPham;
 
     public ArrayList<AnPham> getXmlAnPham() {
