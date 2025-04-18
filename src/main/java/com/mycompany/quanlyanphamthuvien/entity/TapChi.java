6299bdabafb6ca047583b3fc1c5f98584ce9a694
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-04-18 12:01:00 +0700

    Update TapChi.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/entity/TapChi.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/entity/TapChi.java
@@ -2,14 +2,17 @@ package com.mycompany.quanlyanphamthuvien.entity;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "TapChi")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class TapChi extends AnPham {
-
+    @XmlElement(name = "SoPhatHanh")
     private int soPhatHanh;
+    @XmlElement(name = "ThangPhatHanh")
     private int thangPhatHanh;
+    @XmlElement(name = "ChuyenDe")
     private String chuyenDe;
     public TapChi() {
         super();
