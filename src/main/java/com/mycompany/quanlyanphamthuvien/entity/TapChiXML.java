e9569c1f3f936aaf8a7a2f003c50ada43caf7110
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-04-18 12:01:26 +0700

    Update TapChiXML.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/entity/TapChiXML.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/entity/TapChiXML.java
@@ -3,11 +3,15 @@ package com.mycompany.quanlyanphamthuvien.entity;
 import java.util.ArrayList;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "TapChiXMl")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class TapChiXML {
+    @XmlElementWrapper(name = "DanhSachTapChi")
+    @XmlElement(name = "TapChi")
     private ArrayList<TapChi> xmlTapChi;
 
     public ArrayList<TapChi> getXmlTapChi() {
