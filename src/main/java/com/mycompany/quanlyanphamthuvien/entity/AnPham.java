4b66df992e8235dcf64112a4d1db5d894b1f0b6e
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-04-18 11:56:11 +0700

    Update AnPham.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/entity/AnPham.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/entity/AnPham.java
@@ -2,19 +2,31 @@ package com.mycompany.quanlyanphamthuvien.entity;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSeeAlso;
 
 @XmlRootElement(name = "AnPham")
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlSeeAlso({Sach.class, TapChi.class,Bao.class})
+@XmlSeeAlso({Sach.class, TapChi.class, Bao.class})
 public abstract class AnPham {
 
+    @XmlElement(name = "ID", required = true)
     private String ID;
+
+    @XmlElement(name = "TenAnPham", required = true)
     private String tenAnPham;
+
+    @XmlElement(name = "SoLuong")
     private int soLuong;
+
+    @XmlElement(name = "NamXuatBan")
     private int namXuatBan;
+
+    @XmlElement(name = "NhaXuatBan")
     private String nhaXuatBan;
+
+    @XmlElement(name = "GiaTien")
     private double giaTien;
 
     public AnPham() {
@@ -79,3 +91,4 @@ public abstract class AnPham {
 
     public abstract void tinhTrang();
 }
+
