238b35e5b1834b5ca47a8e3658c1296cd431e932
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-04-19 11:02:25 +0700

    Update QuanLyBao.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/action/QuanLyBao.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/action/QuanLyBao.java
@@ -1,14 +1,30 @@
-
 package com.mycompany.quanlyanphamthuvien.action;
 
 import com.mycompany.quanlyanphamthuvien.entity.Bao;
 import com.mycompany.quanlyanphamthuvien.entity.BaoXML;
 import com.mycompany.quanlyanphamthuvien.utils.FileUtils;
 import java.util.ArrayList;
-
+import java.util.Collections;
 
 public class QuanLyBao {
+
     private ArrayList<Bao> qlBao;
+    private QuanLyAnPham qlAnPham;
+
+    public QuanLyBao(QuanLyAnPham qlAnPham) {
+        this.qlBao = docDanhSachBao();
+        this.qlAnPham = qlAnPham;
+        if (qlBao == null) {
+            qlBao = new ArrayList<>();
+        }
+    }
+
+    public QuanLyBao() {
+    }
+
+    public QuanLyAnPham getQlAnPham() {
+        return qlAnPham;
+    }
 
     public ArrayList<Bao> getQlBao() {
         return qlBao;
@@ -17,40 +33,48 @@ public class QuanLyBao {
     public void setQlBao(ArrayList<Bao> qlBao) {
         this.qlBao = qlBao;
     }
-    public void ghiDanhSachBao(ArrayList<Bao> qlBao){
+
+    public void ghiDanhSachBao(ArrayList<Bao> qlBao) {
         BaoXML baoXML = new BaoXML();
         baoXML.setXmlBao(qlBao);
         FileUtils.writeXMLtoFile("Bao.xml", baoXML);
     }
-    public ArrayList<Bao> docDanhSachBao(){
+
+    public ArrayList<Bao> docDanhSachBao() {
         ArrayList<Bao> listBao = new ArrayList<>();
         BaoXML baoXML = (BaoXML) FileUtils.readXMLFile("Bao.xml", BaoXML.class);
-        if(baoXML!=null){
+        if (baoXML != null) {
             listBao = new ArrayList<>(baoXML.getXmlBao());
         }
         return listBao;
     }
-    public void themDtVaoDsBao(Bao baoMoi){
+
+    public void themDtVaoDsBao(Bao baoMoi) {
         qlBao.add(baoMoi);
+        qlAnPham.themDtVaoDsAnPham(baoMoi);
         ghiDanhSachBao(qlBao);
+
     }
-    public boolean xoaDtVaoDsBao(Bao baoXoa){
-        if(baoXoa==null){
+
+    public boolean xoaDtVaoDsBao(Bao baoXoa) {
+        if (baoXoa == null) {
             return false;
         }
-         for(int i = 0 ;i<qlBao.size();i++){
-            if(qlBao.get(i).getID()==baoXoa.getID()){
+        for (int i = 0; i < qlBao.size(); i++) {
+            if (qlBao.get(i).getID() == baoXoa.getID()) {
                 qlBao.remove(i);
+                qlAnPham.themDtVaoDsAnPham(baoXoa);
                 ghiDanhSachBao(qlBao);
+
                 return true;
             }
         }
         return false;
     }
-    public void suaDtDsBao(Bao baoSua)
-    {
-        for(int i = 0 ;i<qlBao.size();i++){
-            if(qlBao.get(i).getID()==baoSua.getID()){
+
+    public void suaDtDsBao(Bao baoSua) {
+        for (int i = 0; i < qlBao.size(); i++) {
+            if (qlBao.get(i).getID() == baoSua.getID()) {
                 qlBao.get(i).setTenAnPham(baoSua.getTenAnPham());
                 qlBao.get(i).setSoLuong(baoSua.getSoLuong());
                 qlBao.get(i).setNamXuatBan(baoSua.getNamXuatBan());
@@ -58,16 +82,22 @@ public class QuanLyBao {
                 qlBao.get(i).setGiaTien(baoSua.getGiaTien());
                 qlBao.get(i).setNgayPhatHanh(baoSua.getNgayPhatHanh());
                 qlBao.get(i).setBienTapVien(baoSua.getBienTapVien());
+                qlAnPham.suaDtDsAnPham(baoSua);
                 ghiDanhSachBao(qlBao);
+
                 break;
-                
+
             }
         }
     }
-    public void sapxepNambao(){
+
+    public void sapxepNambao() {
         Collections.sort(qlBao, (Bao o1, Bao o2) -> Integer.compareUnsigned(o1.getNamXuatBan(), o2.getNamXuatBan()));
     }
-    public void sapxepGiabao(){
-        Collections.sort(qlBao, (Bao o1,Bao o2)-> Double.compare(o1.getGiaTien(), o2.getGiaTien()));
+
+    public void sapxepGiabao() {
+        Collections.sort(qlBao, (Bao o1, Bao o2) -> Double.compare(o1.getGiaTien(), o2.getGiaTien()));
     }
+
 }
+
