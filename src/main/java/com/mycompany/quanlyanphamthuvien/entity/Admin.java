7d383f4237fc67a79ab05be64e6767e6fd82fd27
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-04-20 22:00:46 +0700

    Create Admin.java

---
+++ src/main/java/com/mycompany/quanlyanphamthuvien/entity/Admin.java
@@ -0,0 +1,31 @@
+
+package com.mycompany.quanlyanphamthuvien.entity;
+
+public class Admin {
+    private String tenDangNhap;
+    private String matKhau;
+
+    public Admin() {
+    }
+
+    public Admin(String tenDangNhap, String matKhau) {
+        this.tenDangNhap = tenDangNhap;
+        this.matKhau = matKhau;
+    }
+
+    public String getTenDangNhap() {
+        return tenDangNhap;
+    }
+
+    public void setTenDangNhap(String tenDangNhap) {
+        this.tenDangNhap = tenDangNhap;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+}
