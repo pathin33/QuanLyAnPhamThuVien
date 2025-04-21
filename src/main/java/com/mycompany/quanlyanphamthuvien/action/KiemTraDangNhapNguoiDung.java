f8985c6e731035a3c847ff7273bf285ad7d0de46
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-04-21 07:39:24 +0700

    Create KiemTraDangNhapNguoiDung.java

---
+++ src/main/java/com/mycompany/quanlyanphamthuvien/action/KiemTraDangNhapNguoiDung.java
@@ -0,0 +1,26 @@
+package com.mycompany.quanlyanphamthuvien.action;
+
+import com.mycompany.quanlyanphamthuvien.entity.NguoiDung;
+import com.mycompany.quanlyanphamthuvien.entity.NguoiDungXML;
+import com.mycompany.quanlyanphamthuvien.utils.FileUtils;
+import java.util.ArrayList;
+
+public class KiemTraDangNhapNguoiDung {
+
+    private ArrayList<NguoiDung> dsNguoiDung;
+
+    public void ghiDSNguoiDung(ArrayList<NguoiDung> dsNguoiDung) {
+        NguoiDungXML nguoiDungXML = new NguoiDungXML();
+        nguoiDungXML.setXmlNguoiDung(dsNguoiDung);
+        FileUtils.writeXMLtoFile("NguoiDung.xml", nguoiDungXML);
+    }
+
+    public ArrayList<NguoiDung> docDsNguoiDung() {
+        ArrayList<NguoiDung> listNguoiDung = new ArrayList<>();
+        NguoiDungXML nguoiDungXML = (NguoiDungXML) FileUtils.readXMLFile("Sach.xml", NguoiDungXML.class);
+        if (nguoiDungXML != null) {
+            listNguoiDung = new ArrayList<>(nguoiDungXML.getXmlNguoiDung());
+        }
+        return listNguoiDung;
+    }
+}
