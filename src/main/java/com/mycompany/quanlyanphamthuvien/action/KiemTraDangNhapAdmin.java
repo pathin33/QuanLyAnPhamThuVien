5b986ecf3c9e2ad0bb41c1d7802481cf338a58da
NguyenDuyAnhTuan <153518553+NguyenDuyAnhTuan@users.noreply.github.com>
2025-04-21 20:10:06 +0700

    Update KiemTraDangNhapAdmin.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/action/KiemTraDangNhapAdmin.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/action/KiemTraDangNhapAdmin.java
@@ -1,6 +1,25 @@
 
 package com.mycompany.quanlyanphamthuvien.action;
 
+import com.mycompany.quanlyanphamthuvien.entity.Admin;
+import java.util.List;
+import java.util.ArrayList;
+
 public class KiemTraDangNhapAdmin {
-    
+
+    private List<Admin> danhSachAdmin;
+    public KiemTraDangNhapAdmin() {
+        danhSachAdmin = new ArrayList<>();
+        danhSachAdmin.add(new Admin("admin", "123456"));
+    }
+
+    public boolean kiemTraDangNhap(String tenDangNhap, String matKhau) {
+        for (Admin admin : danhSachAdmin) {
+            if (admin.getTenDangNhap().equals(tenDangNhap) &&
+                admin.getMatKhau().equals(matKhau)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
