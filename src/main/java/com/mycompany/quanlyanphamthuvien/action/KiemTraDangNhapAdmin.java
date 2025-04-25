414109e129997fe9d1ce0166a77540f0c3a59709
NguyenDuyAnhTuan <153518553+NguyenDuyAnhTuan@users.noreply.github.com>
2025-04-26 00:59:57 +0700

    Update KiemTraDangNhapAdmin.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/action/KiemTraDangNhapAdmin.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/action/KiemTraDangNhapAdmin.java
@@ -4,22 +4,14 @@ package com.mycompany.quanlyanphamthuvien.action;
 import com.mycompany.quanlyanphamthuvien.entity.Admin;
 import java.util.List;
 import java.util.ArrayList;
-
 public class KiemTraDangNhapAdmin {
-
-    private List<Admin> danhSachAdmin;
-    public KiemTraDangNhapAdmin() {
-        danhSachAdmin = new ArrayList<>();
-        danhSachAdmin.add(new Admin("admin", "123456"));
-    }
-
-    public boolean kiemTraDangNhap(String tenDangNhap, String matKhau) {
-        for (Admin admin : danhSachAdmin) {
-            if (admin.getTenDangNhap().equals(tenDangNhap) &&
-                admin.getMatKhau().equals(matKhau)) {
+    public boolean checkDangNhapAdmin(Admin admin){
+        if(admin!=null){
+            if("admin".equalsIgnoreCase(admin.getTenDangNhap())&&
+                    "admin".equalsIgnoreCase(admin.getMatKhau()))
                 return true;
-            }
         }
         return false;
     }
 }
+
