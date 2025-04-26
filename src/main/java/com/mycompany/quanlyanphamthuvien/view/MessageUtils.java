27a822cf28aea20747bbd16b8b0eec8c49357e6b
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-04-26 14:01:43 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/quanlyanphamthuvien/view/MessageUtils.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.quanlyanphamthuvien.view;
+
+import javax.swing.JOptionPane;
+
+
+public class MessageUtils {
+    public static void showMessage(java.awt.Component parent, String message) {
+        JOptionPane.showMessageDialog(parent, message);
+    }
+}
