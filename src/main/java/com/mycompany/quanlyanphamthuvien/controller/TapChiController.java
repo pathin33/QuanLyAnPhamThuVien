a82021ab1f81843beeb243954f70013648d2dc22
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-04-28 21:33:36 +0700

    Update TapChiController.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/controller/TapChiController.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/controller/TapChiController.java
@@ -1,25 +1,43 @@
-
 package com.mycompany.quanlyanphamthuvien.controller;
 
 import com.mycompany.quanlyanphamthuvien.action.QuanLyTapChi;
 import com.mycompany.quanlyanphamthuvien.entity.TapChi;
 import com.mycompany.quanlyanphamthuvien.view.QLtapchi;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-
 public class TapChiController {
-    private QLtapchi qlTapChiView ;
+
+    private QLtapchi qlTapChiView;
     private QuanLyTapChi qlTapChi;
 
     public TapChiController(QLtapchi qlTapChiView) {
         this.qlTapChi = new QuanLyTapChi();
         this.qlTapChiView = qlTapChiView;
+        qlTapChiView.buttonAddTapChi(new addControllerTapChi());
     }
-    
-    public void bangTapChi(){
+
+    public void bangTapChi() {
         ArrayList<TapChi> tapChi = new ArrayList<>();
         tapChi = qlTapChi.docDanhSachTapChi();
         qlTapChiView.setVisible(true);
         qlTapChiView.khoiTaoBangTapChi(tapChi);
+    }
+
+    class addControllerTapChi implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            TapChi tapChi = qlTapChiView.checkDuLieuHopLe();
+            if (tapChi != null) {
+                qlTapChi.themDtVaoDsTapChi(tapChi);
+                ArrayList<TapChi> dsTapChi = qlTapChi.docDanhSachTapChi();
+
+                qlTapChiView.khoiTaoBangTapChi(dsTapChi);
+
+                qlTapChiView.showMessage("Thêm tạp chí thành công!");
+            }
+        }
     }
 }
