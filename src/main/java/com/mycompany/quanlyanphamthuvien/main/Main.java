958b4b30d26176e1ce853d4ebb9b60975bdfbc17
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-05-04 17:00:12 +0700

    Update Main.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/main/Main.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/main/Main.java
@@ -1,9 +1,43 @@
-
 package com.mycompany.quanlyanphamthuvien.main;
 
+import com.mycompany.quanlyanphamthuvien.view.Login;
+
+import javax.swing.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception fallbackEx) {
+                fallbackEx.printStackTrace();
+            }
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            Login login = new Login();
+            updateLookAndFeel(login);
+            login.setLocationRelativeTo(null);
+            login.setVisible(true);
+        });
+    }
+
+    public static void updateLookAndFeel(JFrame frame) {
+        SwingUtilities.updateComponentTreeUI(frame);
+        frame.pack();
+    }
+
+    public static void updateLookAndFeel(JPanel panel) {
+        SwingUtilities.updateComponentTreeUI(panel);
+        panel.revalidate();
+        panel.repaint();
     }
 }
