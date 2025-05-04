89257e783ccd2194492b6bf2342e019e95276af9
Thien Nguyen <150871778+pathin33@users.noreply.github.com>
2025-05-04 16:58:51 +0700

    Update SachController.java

--- src/main/java/com/mycompany/quanlyanphamthuvien/controller/SachController.java
+++ src/main/java/com/mycompany/quanlyanphamthuvien/controller/SachController.java
@@ -1,174 +1,141 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.quanlyanphamthuvien.controller;
 
+import com.mycompany.quanlyanphamthuvien.action.QuanLySach;
 import com.mycompany.quanlyanphamthuvien.entity.Sach;
-import com.mycompany.quanlyanphamthuvien.view.Login;
 import com.mycompany.quanlyanphamthuvien.view.QLsach;
-import com.mycompany.quanlyanphamthuvien.view.QuanLyMain;
-import com.mycompany.quanlyanphamthuvien.view.SignUpUser;
-import com.mycompany.quanlyanphamthuvien.view.User;
-import com.mycompany.quanlyanphamthuvien.view.timkiemsach;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JOptionPane;
-import com.mycompany.quanlyanphamthuvien.action.QuanLySach;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-/**
- *
- * @author Admin
- */
 public class SachController {
-    private ArrayList<Sach> book;
-    private QLsach qlsachview;
-    private QuanLySach quanlySach;
 
-    public SachController(QLsach qlsachview) {
-        this.quanlySach = new QuanLySach();
-        this.qlsachview = qlsachview;
-        addEventListeners();
+    private QLsach qlSachView;
+    private QuanLySach qlSach;
+
+    public SachController(QLsach qlSachView) {
+        this.qlSach = QuanLySach.getInstance();
+        this.qlSachView = qlSachView;
+
+        qlSachView.buttonAddSach(new AddControllerSach());
+        qlSachView.bangDsSach(new ChonHangLayChiSo());
+        qlSachView.xoaDt(new XoaDtSach());
+        qlSachView.suaDt(new SuaDtSach());
+        qlSachView.sapXepTheoTen(new SapXepTenSach());
+        qlSachView.sapXepTheoSoLuong(new SapXepSoLuongSach());
+        qlSachView.sapXepTheoGiaTien(new SapXepGiaTienSach());
+        qlSachView.sapXepTheoNam(new SapXepNamSach());
+        qlSachView.cancelSearch(new CancelSearch());
     }
 
-    private void addEventListeners() {
-        qlsachview.addUndoListener(new UndoListener());
-        qlsachview.addAddListener(new AddBookListener());      
-        qlsachview.addEditListener(new suaDtSach());
-        qlsachview.addDeleteListener(new xoaDtSach());
-        qlsachview.addListSelectionListener(new chonHangLayChiSo());
-        qlsachview.addSortTitleListener(new sapxepTenSach());
-        qlsachview.addSortQuantityListener(new sapxepSoLuongSach());
-        qlsachview.addSortPriceListener(new sapxepGiatien());
-        qlsachview.addSortYearListener(new sapxepNam());
-        
-    }
     public void bangSach() {
-        ArrayList<Sach> sach = new ArrayList<>();
-        sach = quanlySach.docDanhSachSach();
-        qlsachview.setVisible(true);
-        qlsachview.khoiTaoBangSach(sach);
+        ArrayList<Sach> sach = qlSach.docDanhSachSach();
+        qlSachView.setVisible(true);
+        qlSachView.khoiTaoBangSach(sach);
     }
-    class UndoListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            qlsachview.dispose();
-            new DieuKhienCacQuanLy().showMainView();
-        }
-    }
-    class AddBookListener implements ActionListener { 
+
+    class AddControllerSach implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            Sach newsach = qlsachview.getSachInfo();
-            if (newsach != null) {
-                quanlySach.themDtVaoDsSach(newsach);
-                ArrayList<Sach> dssach = quanlySach.docDanhSachSach();
-                qlsachview.khoiTaoBangSach(dssach); 
-                qlsachview.showMessage("Thêm sách thành công!");
-            } else {
-                qlsachview.showMessage("Vui lòng nhập đầy đủ thông tin sách!");
+            Sach sach = qlSachView.checkDuLieuHopLe();
+            if (sach != null) {
+                if (qlSach.kiemTraTrungID(sach)) {
+                    qlSach.themDtVaoDsSach(sach);
+                    ArrayList<Sach> dsSach = qlSach.docDanhSachSach();
+                    qlSachView.khoiTaoBangSach(dsSach);
+                    qlSachView.showMessage("Book added successfully!");
+                } else {
+                    qlSachView.showMessage("Book ID already exists!");
+                }
             }
         }
     }
-    class chonHangLayChiSo implements ListSelectionListener {
 
+    class ChonHangLayChiSo implements ListSelectionListener {
         @Override
         public void valueChanged(ListSelectionEvent e) {
             try {
-                qlsachview.laychiso();
+                qlSachView.layChiSo();
             } catch (ParseException ex) {
                 Logger.getLogger(SachController.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-
     }
 
-    class xoaDtSach implements ActionListener {
+    class XoaDtSach implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            qlsachview.xoaDt();
+            qlSachView.xoaDt();
         }
-
     }
 
-    class suaDtSach implements ActionListener {
-
+    class SuaDtSach implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            Sach sach = qlsachview.getSachInfo2();
+            Sach sach = qlSachView.checkDuLieuTruID();
             if (sach != null) {
-                quanlySach.suaDtDsSach(sach);
-                ArrayList<Sach> dsSach = quanlySach.docDanhSachSach();
-
-                qlsachview.khoiTaoBangSach(dsSach);
-
-                qlsachview.showMessage("Sửa Sách thành công!");
-
+                qlSach.suaDtDsSach(sach);
+                ArrayList<Sach> dsSach = qlSach.docDanhSachSach();
+                qlSachView.khoiTaoBangSach(dsSach);
+                qlSachView.showMessage("Book updated successfully!");
             }
         }
-
     }
 
-    class sapxepTenSach implements ActionListener {
-
+    class SapXepTenSach implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-
-            ArrayList<Sach> danhSachMoi = quanlySach.docDanhSachSach();
-            quanlySach.getListSach().clear();
-            quanlySach.getListSach().addAll(danhSachMoi);
-            quanlySach.sapXepTenSach();
-            qlsachview.khoiTaoBangSach(quanlySach.getListSach());
+            ArrayList<Sach> danhSachMoi = qlSach.docDanhSachSach();
+            qlSach.getQlSach().clear();
+            qlSach.getQlSach().addAll(danhSachMoi);
+            qlSach.sapXepTenSach();
+            qlSachView.khoiTaoBangSach(qlSach.getQlSach());
         }
     }
 
-    class sapxepSoLuongSach implements ActionListener {
-
+    class SapXepSoLuongSach implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            ArrayList<Sach> danhSachMoi = quanlySach.docDanhSachSach();
-            quanlySach.getListSach().clear();
-            quanlySach.getListSach().addAll(danhSachMoi);
-            quanlySach.sapxepSoLuong();
-            qlsachview.khoiTaoBangSach(quanlySach.getListSach());
+            ArrayList<Sach> danhSachMoi = qlSach.docDanhSachSach();
+            qlSach.getQlSach().clear();
+            qlSach.getQlSach().addAll(danhSachMoi);
+            qlSach.sapXepSoLuongSach();
+            qlSachView.khoiTaoBangSach(qlSach.getQlSach());
         }
     }
 
-    class sapxepGiatien implements ActionListener {
+    class SapXepGiaTienSach implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            ArrayList<Sach> danhSachMoi = quanlySach.docDanhSachSach();
-            quanlySach.getListSach().clear();
-            quanlySach.getListSach().addAll(danhSachMoi);
-            quanlySach.sapxepGiasach();
-            qlsachview.khoiTaoBangSach(quanlySach.getListSach());
+            ArrayList<Sach> danhSachMoi = qlSach.docDanhSachSach();
+            qlSach.getQlSach().clear();
+            qlSach.getQlSach().addAll(danhSachMoi);
+            qlSach.sapxepGiasach();
+            qlSachView.khoiTaoBangSach(qlSach.getQlSach());
         }
     }
 
-    class sapxepNam implements ActionListener {
-
+    class SapXepNamSach implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            ArrayList<Sach> danhSachMoi = quanlySach.docDanhSachSach();
-
-
-            quanlySach.getListSach().clear();
-            quanlySach.getListSach().addAll(danhSachMoi);
-            quanlySach.sapxepNamsach();
-            qlsachview.khoiTaoBangSach(quanlySach.getListSach());
+            ArrayList<Sach> danhSachMoi = qlSach.docDanhSachSach();
+            qlSach.getQlSach().clear();
+            qlSach.getQlSach().addAll(danhSachMoi);
+            qlSach.sapxepNamsach();
+            qlSachView.khoiTaoBangSach(qlSach.getQlSach());
         }
     }
-    class timKiem implements ActionListener {
+
+    class CancelSearch implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            
+            ArrayList<Sach> dsSach = qlSach.docDanhSachSach();
+            qlSachView.khoiTaoBangSach(dsSach);
         }
     }
-    
+
 }
